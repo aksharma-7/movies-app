28b0d10e4e59a1ee984c6b8f51aa99b5f6ccb07e
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-01-10 21:19:25 +0530

    Added wrapper classes

--- src/components/App.js
+++ src/components/App.js
@@ -43,37 +43,37 @@ class App extends React.Component {
 
 
     return (
-      <StoreContext.Consumer>
-        {(store) => {
-          return (
-            <div className="App">
-              <Navbar dispatch={this.props.store.dispatch} search={search} />
-              <div className="main">
-                <div className="tabs">
-                  <div className={`tab ${showFavourites ? '': 'active-tabs'}`} onClick={() => this.onChangeTab(false)}>Movies</div>
-                  <div className={`tab ${showFavourites ? 'active-tabs': ''}`} onClick={() => this.onChangeTab(true)}>Favorites</div>
-                </div>
-                
-                <div className="list">
-                  {displayMovies.map((movie, index) => (
-                    <MovieCard 
-                      movie={movie} 
-                      key={`movies-${index}`} 
-                      dispatch={this.props.store.dispatch}
-                      isFavourite={this.isMovieFavourite(movie)}
-                    />
-                  ))}
-                </div>
-                {displayMovies.length === 0 ? <div className="no-movies">No movies to display</div> : null}
-              </div>
-            </div>
-        );
-        }}
-      </StoreContext.Consumer>
-    )
-
-    
+      <div className="App">
+        <Navbar search={search} />
+        <div className="main">
+          <div className="tabs">
+            <div className={`tab ${showFavourites ? '': 'active-tabs'}`} onClick={() => this.onChangeTab(false)}>Movies</div>
+            <div className={`tab ${showFavourites ? 'active-tabs': ''}`} onClick={() => this.onChangeTab(true)}>Favorites</div>
+          </div>
+          
+          <div className="list">
+            {displayMovies.map((movie, index) => (
+              <MovieCard 
+                movie={movie} 
+                key={`movies-${index}`} 
+                dispatch={this.props.store.dispatch}
+                isFavourite={this.isMovieFavourite(movie)}
+              />
+            ))}
+          </div>
+          {displayMovies.length === 0 ? <div className="no-movies">No movies to display</div> : null}
+        </div>
+      </div>
+  );
   }
 }
 
-export default App;
+class AppWrapper extends React.Component {
+  render () {
+    return <StoreContext.Consumer>
+      {(store) => <App store={store} />}
+    </StoreContext.Consumer>
+  }
+}
+
+export default AppWrapper;
